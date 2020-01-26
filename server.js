9af6b402e1d536ed8fe8f7902d8cd14083165dc9
Gage D'Orlando <gagedorlando@gmail.com>
2020-01-26 10:48:19 -0500

    Finally fixed pictures

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes")
 
+
   app.use(express.json());
   if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
   app.use(routes)
-
+  app.use(express.urlencoded({ extended: true }));
 app.listen(PORT, function(){
     console.log(`Now listening on ${PORT}`)
 })
