78a0d8c83ec4638a723f963a9da193d74c0b5156
Gage D'Orlando <gagedorlando@gmail.com>
2020-01-26 04:44:14 -0500

    work

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes")
 
+app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("updated-porfolio/build"));
+  }
   app.use(routes)
 
 app.listen(PORT, function(){
