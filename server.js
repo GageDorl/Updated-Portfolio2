be4716dbf208607165cd9548489d076d819f1682
Gage D'Orlando <gagedorlando@gmail.com>
2020-01-26 10:43:53 -0500

    Finally fixed pictures

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes")
 
-app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
