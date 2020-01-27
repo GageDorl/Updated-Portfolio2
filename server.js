dea80500660b99ef80cb1bf275b771f169405226
Gage D'Orlando <gagedorlando@gmail.com>
2020-01-26 20:32:13 -0500

    Fix all the errors

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const routes = require("./routes")
   if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
-  app.use("/api",routes);
+  
   const frontend = path.join(__dirname, 'client/build');
 
 // map the requests to the static react build directory
-  app.use('/', express.static(frontend));
-  
+  app.use('/projects', express.static(frontend));
+  app.use('/contact', express.static(frontend));
 // all the unknown requests are redirected to the react SPA
   app.use(function (req, res, next) {
     res.sendFile(path.join(frontend, 'index.html'));
